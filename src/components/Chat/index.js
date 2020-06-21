646423eb4c193f3f2ae745088fd7f28cdd15c650
Ignacio De La Madrid <ignacio.delamadrid@gmail.com>
2020-06-20 19:42:28 -0500

    Added scrolling hooks

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import moment from 'moment'
 import styles from './styles.module.scss'
 import Message from './Message'
@@ -19,6 +19,16 @@ const buildMessage = ({
 }
 
 const Chat = (props) => {
+  const chatWrapperRef = useRef()
+  const chatContentRef = useRef()
+
+  console.log('Wrapper Offsetheight', chatWrapperRef.current?.offsetHeight)
+  console.log('Content Offsetheight', chatContentRef.current?.offsetHeight)
+
+  console.log('Wrapper Scrollheight', chatWrapperRef.current?.scrollHeight)
+  console.log('Content scrollHeight', chatContentRef.current?.scrollHeight)
+  console.log('Content scrollTop', chatContentRef.current?.scrollTop)
+
   const [currentMessageText, setCurrentMessageText] = useState('')
 
   const [messages, setMessages] = useState([
@@ -69,18 +79,32 @@ const Chat = (props) => {
     },
   ])
 
-  const appendMessage = (evt) => {
-    evt.preventDefault()
-    const newMessage = buildMessage({ text: currentMessageText })
-    setMessages([newMessage, ...messages])
-  }
+  const appendMessage = useCallback(
+    (evt) => {
+      evt.preventDefault()
+      const newMessage = buildMessage({ text: currentMessageText })
+      setMessages([...messages, newMessage])
+      setCurrentMessageText('')
+    },
+    [currentMessageText, messages, setCurrentMessageText, setMessages]
+  )
 
   const onCurrentMessageTextChange = (evt) =>
     setCurrentMessageText(evt.target.value)
 
+  const scrollTop = () => {
+    const chatContentDom =
+      chatContentRef && chatContentRef.current ? chatContentRef.current : null
+    if (chatContentDom) chatContentDom.scrollTop = chatContentDom.scrollHeight
+  }
+
+  useEffect(() => {
+    scrollTop()
+  }, [scrollTop])
+
   return (
-    <div className={styles.container}>
-      <div className={styles.chatContainer}>
+    <div className={styles.container} ref={chatWrapperRef}>
+      <div className={styles.chatContainer} ref={chatContentRef}>
         {messages.map((message, index) => (
           <Message
             key={`${index}-{message.src}`}
